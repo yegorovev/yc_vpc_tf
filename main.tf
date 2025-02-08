302841d916f1b8aca58b453bcfe810ae838c1104
ee <ee@email.com>
2025-02-08 21:42:36 +0300

    Required provider was added

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    } 
+  } 
+}
+
 resource "yandex_vpc_network" "vpc" {
   name   = var.vpc_name
   labels = var.vpc_labels
